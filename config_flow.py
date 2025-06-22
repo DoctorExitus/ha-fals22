360f1ac29ee2c15960820a409db97e6e48ee3fa8
DoctorExitus <48226045+DoctorExitus@users.noreply.github.com>
2025-06-22 15:56:08 +0200

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -75,7 +75,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     # Return info that you want to store in the config entry.
     return {
-        "title": f"FALS22 ({host})",
+        "title": f"FaLs22 ({host})",
         "host": host,
         "password": password,
     }
